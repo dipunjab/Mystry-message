2f919517010e2ed0a74cd320eb08f05fd83a8b07
Usman <ghaniusman0287@gmail.com>
2025-08-18 23:02:22 +0500

    feat: implement DELETE API for message removal with authentication checks

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,59 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/modal/User";
+import { User } from "next-auth";
+
+
+export async function DELETE(request: Request, {params}: {params: {messageid: string}}) {
+    const messageid = params.messageid;
+    await dbConnect();
+
+     const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "Not authenticated"
+            },
+            { status: 401 }
+        )
+    }
+
+    try {
+        const updatedResult = await UserModel.updateOne(
+            {id: user._id},
+            {$pull: {messages: {_id: messageid}}}
+        );
+        if (updatedResult.modifiedCount ==0 ) {
+            return Response.json(
+            {
+                success: false,
+                message: "Message not found or already deleted"
+            },
+            { status: 404 }
+        )
+        }
+
+        return Response.json(
+            {
+                success: false,
+                message: "Message Deleted"
+            },
+            { status: 200 }
+        )
+    } catch (error) {
+        console.log("Error deleting message", error);
+        
+        return Response.json(
+            {
+                success: false,
+                message: "Error deleteing message"
+            },
+            { status: 500 }
+        )
+    }
+
+}
